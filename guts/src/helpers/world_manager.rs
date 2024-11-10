0b4f85970168f2e8c8024c76a7d0958b4d75169d
CharmanderTheDev <144705748+CharmanderTheDev@users.noreply.github.com>
2024-11-09 18:15:27 -0600

    Computer additions (smal)

--- guts/src/helpers/world_manager.rs
+++ guts/src/helpers/world_manager.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use crate::helpers::directory_managers::Directory;
 
 pub trait Entity {
     
@@ -7,11 +8,19 @@ pub trait Entity {
 
 pub struct Computer<'a> {
 
+    pub directory: Directory,
     
+    pub current_memory: i32,
+    pub max_memory: i32,
+    
+    pub current_processes: i32,
+    pub max_processes: i32,
+    
+    pub processing_speed: i32,
     
 } impl<'a> Computer<'_> {
 
-    
+    pub fn new(max_processes: i32, 
 
 } impl Entity for Computer {
 
